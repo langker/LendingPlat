5e4607e5c01c327bb8f7c678a61e58c02c57f865
langker <langker@aliyun.com>
2017-12-07 15:41:20 +0100

    fix bug in rentdlg

--- src/main/java/me/langker/LendingPlat/Viewer/ProductViewer.java
+++ src/main/java/me/langker/LendingPlat/Viewer/ProductViewer.java
@@ -3,14 +3,17 @@ package me.langker.LendingPlat.Viewer;
 import java.io.IOException;
 import java.util.Date;
 
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
 import javax.inject.Inject;
+
+import org.primefaces.context.RequestContext;
+
 import me.langker.LendingPlat.Controller.ContractController;
 import me.langker.LendingPlat.Controller.ProductController;
-import me.langker.LendingPlat.Dao.ProductDao;
 import me.langker.LendingPlat.Entity.Product;
 
 @ManagedBean(name = "product")
@@ -23,11 +26,15 @@ public class ProductViewer {
 	@Inject ProductController pController;
 	@Inject ContractController contractController;
 	public void getProduct(String id) throws IOException { 
-		if (id.equals("")==false) {
-			p = pController.findProductById(Integer.valueOf(id));
-			if (p==null) FacesContext.getCurrentInstance().getExternalContext().redirect("index.xhtml");
+		if (FacesContext.getCurrentInstance().getPartialViewContext().isAjaxRequest()) { 
+			return;
 		} else {
-			FacesContext.getCurrentInstance().getExternalContext().redirect("index.xhtml");
+			if (id.equals("")==false) {
+				p = pController.findProductById(Integer.valueOf(id));
+				if (p==null) FacesContext.getCurrentInstance().getExternalContext().redirect("index.xhtml");
+			} else {
+				FacesContext.getCurrentInstance().getExternalContext().redirect("index.xhtml");
+			}
 		}
 	}
 	public Product getP() {
@@ -38,8 +45,20 @@ public class ProductViewer {
 		this.p = p;
 	}
 	public void rentProduct(String pid, String uid) {
-		pController.setNewAvailableDate(Integer.valueOf(pid), term, startRentingdate);
-		contractController.addContract(Integer.valueOf(pid),Integer.valueOf(uid),term,startRentingdate);
+		if (startRentingdate!=null&&term!=0) {
+			if(startRentingdate.before(p.getAvailableDate())==true) {
+				FacesContext.getCurrentInstance().addMessage(null, 
+						new FacesMessage(FacesMessage.SEVERITY_FATAL, "Fatal!", "the date you choose should after the available date!"));
+				
+			} else {
+				pController.setNewAvailableDate(Integer.valueOf(pid), term, startRentingdate);
+				contractController.addContract(Integer.valueOf(pid),Integer.valueOf(uid),term,startRentingdate);
+				RequestContext.getCurrentInstance().execute("alert('rent successful');PF('rentDlg').hide()");
+			}
+		} else {
+			FacesContext.getCurrentInstance().addMessage(null,
+	                new FacesMessage(FacesMessage.SEVERITY_ERROR,"ERROR: you should input",""));
+		}
 	}
 	public Date getStartRentingdate() {
 		return startRentingdate;
